a0bb794dfbc405738182dcad311ebcc729038c2a
JeanBrice <jeanbrice.canihac@gmail.com>
2016-09-06 15:50:06 +0200

    Test push

--- core/src/main/java/core/AddingMachine.java
+++ core/src/main/java/core/AddingMachine.java
@@ -22,7 +22,7 @@ public final class AddingMachine implements ICalculationMachine {
 
     @Override
     public double calculate(double a, double b) {
-        return a + b; // test pd
+        return a + b; // Allan suce des chibres et il kiffe ça
     }
 
 }
